9132d0787df2661a784304853dfb5a77256cc53b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-14 20:31:52 +0100

    Infinix-X676C: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: Infinix X676C Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X676C.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X676C-user \
+    omni_Infinix-X676C-userdebug \
+    omni_Infinix-X676C-eng
